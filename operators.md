111db11fc6ea13623951a07236ae2b0a224b2526
Vedant <vedantbakshi07@gmial.com>
2022-09-15 22:25:47 +0530

    initial

---
+++ operators.md
@@ -0,0 +1,57 @@
+BITWISE OR ( | )
+a = 5 = 0101 (In Binary)
+b = 7 = 0111 (In Binary)
+
+Bitwise OR Operation of 5 and 7
+  0101
+| 0111
+ ________
+  0111  = 7 (In decimal) 
+
+
+
+
+BITWISE AND ( & )
+a = 5 = 0101 (In Binary)
+b = 7 = 0111 (In Binary)
+
+Bitwise AND Operation of 5 and 7
+  0101
+& 0111
+ ________
+  0101  = 5 (In decimal) 
+
+
+
+
+
+BITWISE XOR ( ^ )
+a = 5 = 0101 (In Binary)
+b = 7 = 0111 (In Binary)
+
+Bitwise XOR Operation of 5 and 7
+  0101
+^ 0111
+ ________
+  0010  = 2 (In decimal) 
+
+
+
+
+
+Bitwise Complement (~)
+a = 5 = 0101 (In Binary)
+
+Bitwise Complement Operation of 5
+
+~ 0101
+ ________
+  1010  = 10 (In decimal) 
+Note: Compiler will give 2’s complement of that number, i.e., 2’s complement of 10 will be -6.
+
+
+
+
+
+
+
---
+++ pseudocode.py
@@ -0,0 +1,102 @@
+# p = 22
+# q = 12
+# def fun(p,q):
+#     if p==1:
+#         return q
+#     else:
+#         if p%2==0:
+#             return fun(p-1,q)
+#         else:
+#             return 0
+# print(fun(p,q))
+#output ----  0
+
+
+
+
+
+# a = 0
+# for i in range(0,3):
+#     a = a + i
+#     if i==3:
+#      print("hello")
+
+
+# a = 'a'
+# b = 'b'
+# res = a + b
+# print(res)
+
+
+
+# c=1
+# def fun(c):
+#     print(c)
+#     if c<3:
+#         c =c+2
+#         fun(fun (c))
+
+#     return c
+# print(fun(c))
+
+
+
+
+# n = 1
+
+# def fun(n):
+#       i =1
+#       if n>=5:
+#         return n
+#       n=n+1
+#       i+=1
+#       return fun(n)
+# print(fun(n))
+
+
+
+# p = 2
+# q = 5
+
+# def fun(p,q):
+#     if p==0:
+#         return q
+#     return fun(p-1,p+q)
+# print(fun(p,q))
+
+
+
+
+# c = 1
+# def fun(c):
+#     print(c)
+#     if c<3:
+#         c =c+2
+#         fun(fun (c))
+#     return c
+# print(fun(c))  
+
+
+
+
+# n = 5
+# f0=0
+# f1=3
+# for f in (1,n):
+#     f = f0+f1
+#     print(f)
+#     f0=f1
+#     f1=f
+
+
+
+# def modify(w,u):
+#     w = w + 2#10+2=12
+#     u = u - 3
+#     return(w-u)
+# def calculate():
+#     a = 10
+#     b = 20
+#     c = modify(a,b)
+#     print(a)#10
+#     print(b)#20
