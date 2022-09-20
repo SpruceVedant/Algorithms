a30d070c21e76956ad951d2a7107e0a51dadd924
SpruceVedant <76097198+SpruceVedant@users.noreply.github.com>
2022-09-20 13:17:09 +0530

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,18 @@
+nterms = int(input("enter nos"))
+n1= 0
+n2= 1
+count = 0
+
+if nterms <= 0:
+   print("enter positive no")
+elif:
+    print(n1)
+else:
+    while count < nterms:
+       print(n1)
+       n = n1 + n2
+       n1 = n2
+       n2 = n
+       count += 1
+      
+
