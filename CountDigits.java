f6755e99c3717ec9ff2c839c108017869546011c
SpruceVedant <76097198+SpruceVedant@users.noreply.github.com>
2022-09-27 15:55:23 +0530

    Update CountDigits.java

--- CountDigits.java
+++ CountDigits.java
@@ -2,13 +2,13 @@
 
 class countdigits{
   public static void main(String[] args){
-    int n = 12
+    int n = 12;
     int count = 0;
     int temp = n;
     while(temp > 0){
       int rem = temp % 10;
       if(rem != 0 && n % rem == 0){
-        count++
+        count++;
       }
       temp = temp / 10;
     }
