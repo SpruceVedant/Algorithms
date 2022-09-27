fae7bb637e0a7e8735973a2f753c4d9b2711fe57
SpruceVedant <76097198+SpruceVedant@users.noreply.github.com>
2022-09-27 15:50:13 +0530

    Update LongestCommonSubstring.java

--- LongestCommonSubstring.java
+++ LongestCommonSubstring.java
@@ -2,7 +2,7 @@ class longest{
    public int LongestCommonSubstring(String s){
     int maxlength = 0;
      for(int i=0;i<s.length();i++){
-        Stringbuilder currentsubstring = new Stringbuilder();
+        StringBuilder currentsubstring = new StringBuilder();
         for(int j=i;j<s.length();j++){
             if(currentsubstring.indexOf(String.valueOf(s.charAt(j))) != -1){
                 break;
@@ -11,6 +11,7 @@ class longest{
             maxlength = Math.max(maxlength, currentsubstring.length());
         }
      }
+    return maxlength;
    }
-   return maxlength;
+   
 }
