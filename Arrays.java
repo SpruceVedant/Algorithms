13cae5bcbbd7ecb1efe75cfd19d0046ad96db5fb
SpruceVedant <76097198+SpruceVedant@users.noreply.github.com>
2022-09-27 15:54:46 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
 class arrays{
-  int arr[] = {1,2,3,4};
+
   public static void main(String[],args){
+      int arr[] = {1,2,3,4};
 //     TO SORT ARRAY
     Arrays.sort(arr);
   for(int i=0;i<arr.length;i++){
