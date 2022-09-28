bb4e97b601a87bf480b4d0f8d2e1db95c2957813
SpruceVedant <76097198+SpruceVedant@users.noreply.github.com>
2022-09-28 18:58:40 +0530

    Another approach using java inbuilt methods

--- SquaresofSortedArrays.java
+++ SquaresofSortedArrays.java
@@ -18,3 +18,33 @@ class Solution {
         return result;
     }
 }
+
+
+
+
+
+
+
+
+// import java.util.Arrays;
+
+// // Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+// class dfsdf {
+//     public int[] sortedSquares(int[] nums) {
+//         int[] result = new int[nums.length];
+//         for(int i = 0; i < nums.length; i++) {
+//             result[i] = nums[i] * nums[i];
+//         }
+//         Arrays.sort(result);
+//         return result;
+        
+//     }
+//     public static void main(String[] args) {
+//         dfsdf s = new dfsdf();
+//         int[] nums = {-4,-1,0,3,10};
+//         int[] result = s.sortedSquares(nums);
+//         for (int i = 0; i < result.length; i++) {
+//             System.out.println(result[i]);
+//         }
+//     }
+// }
