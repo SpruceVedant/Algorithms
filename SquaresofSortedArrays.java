c02013b625dfed994f8efcfae9eba4539816c633
SpruceVedant <76097198+SpruceVedant@users.noreply.github.com>
2022-09-28 18:57:49 +0530

    Create SquaresofSortedArrays.java

---
+++ SquaresofSortedArrays.java
@@ -0,0 +1,20 @@
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+         int[] result = new int[nums.length];
+        int i = 0;
+        int j = nums.length - 1;
+        int k = nums.length - 1;
+        while (i <= j) {
+            if (nums[i] * nums[i] > nums[j] * nums[j]) {
+                result[k] = nums[i] * nums[i];
+                i++;
+            } else {
+                result[k] = nums[j] * nums[j];
+                j--;
+            }
+            k--;
+        }
+        return result;
+    }
+}
