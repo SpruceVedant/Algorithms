34b511ed6398f58fb2b787f2343a1d552f887dbe
SpruceVedant <76097198+SpruceVedant@users.noreply.github.com>
2022-10-17 18:22:48 +0530

    Update SlidingWindowPRoblem.java

--- SlidingWindowPRoblem.java
+++ SlidingWindowPRoblem.java
@@ -33,3 +33,37 @@
         System.out.println(sum);
     }
 }
+
+// Sliding Window Technique: The technique can be best understood with the window pane in bus, consider a window of length n and the pane which is fixed in it of length k. Consider, initially the pane is at extreme left i.e., at 0 units from the left. Now, co-relate the window with array arr[] of size n and pane with current_sum of size k elements. Now, if we apply force on the window such that it moves a unit distance ahead. The pane will cover next k consecutive elements. 
+
+// Applying sliding window technique : 
+
+// We compute the sum of first k elements out of n terms using a linear loop and store the sum in variable window_sum.
+// Then we will graze linearly over the array till it reaches the end and simultaneously keep track of maximum sum.
+// To get the current sum of block of k elements just subtract the first element from the previous block and add the last element of the current block .
+
+
+
+ class slidingwindow {
+    public static void main(String[] args) {
+        int arr[] = {100,200,300,400};
+        int k = 2;
+        
+        if(arr.length < k){
+            System.out.println("Invalid");
+            return;
+        }
+        int max_sum = 0;
+        for (int i = 0; i < k; i++) {
+            max_sum += arr[i];
+
+            int wsum = max_sum;
+            for (int i = k; i < arr.length; i++) {
+                wsum += arr[i] - arr[i - k];
+                max_sum = Math.max(max_sum, wsum);
+            }
+        }
+        System.out.println(max_sum);
+    }
+}
+
