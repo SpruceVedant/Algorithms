1ba76e7479eb85028afbb9f94f386d13c036c4c1
SpruceVedant <76097198+SpruceVedant@users.noreply.github.com>
2022-11-11 21:45:16 +0530

    Create PivotIndex.java

---
+++ PivotIndex.java
@@ -0,0 +1,16 @@
+class pivot{
+  public int pivotindex(int nums[]){
+    int sum = 0;
+    int left_sum = 0;
+    
+    for(int x : nums){
+      sum += x;
+    }
+    for(int i=0;i<nums.length;i++){
+      if(left_sum = sum - left_sum - nums[i]) return i;
+      
+      left_sum += nums[i]; 
+    }
+    return -1;
+  }
+}
