693f0fdbd748b695c1a8c61a20aaa1845d6f570f
SpruceVedant <76097198+SpruceVedant@users.noreply.github.com>
2022-11-13 16:02:20 +0530

    Create isSubsequence.java

---
+++ isSubsequence.java
@@ -0,0 +1,12 @@
+class sub{
+  public boolean isSequence(String s, String t){
+    int i = 0;
+    int j = 0;
+    while(i < s.length() && j < t.length()){
+      i++;
+    }
+    j++;
+  }
+  return i==s.length();
+ }
+}
