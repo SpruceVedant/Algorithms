a2961ab5e6973d50eba95d20330a3641416b9de3
SpruceVedant <76097198+SpruceVedant@users.noreply.github.com>
2022-11-14 17:00:58 +0530

    Create StringReverse.java

---
+++ StringReverse.java
@@ -0,0 +1,13 @@
+ class reversestring {
+    public static void main(String[] args) {
+        String s = "Hello";
+        String rev = "";
+        StringBuilder sb = new StringBuilder(s);
+        sb.append(s);
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
+
+
+
