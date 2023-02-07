96e33f5b9714dec5c58a5f1eb223b1e108f176f7
Vedant Bakshi <76097198+SpruceVedant@users.noreply.github.com>
2023-02-07 21:50:29 +0530

    Added leetcode problem of the day

---
+++ Fruits_In_The_Basket.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+      int maxcount = 0;
+      for(int i=0;i<fruits.length;i++){
+          // set = basket
+          HashSet<Integer> set = new HashSet<>();
+          int j = i;
+
+          while(j<fruits.length){
+              if(!set.contains(fruits[j]) && set.size() == 2)
+              break;
+              
+              set.add(fruits[j]);
+              j++;
+          }
+          maxcount = Math.max(maxcount,j-i);
+      }
+      return maxcount;
+
+    }
+}
