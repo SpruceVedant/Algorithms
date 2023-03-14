cad28f1c3a0efa9488e876d20cdd87f9ce792d3a
Vedant Bakshi <76097198+SpruceVedant@users.noreply.github.com>
2023-03-14 22:56:39 +0530

    Leetcode #129 Medium
    Tree recursion

---
+++ Sum_Root_to_Leaf_Numbers.java
@@ -0,0 +1,41 @@
+// You are given the root of a binary tree containing digits from 0 to 9 only.
+
+// Each root-to-leaf path in the tree represents a number.
+
+// For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+// Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+// A leaf node is a node with no children.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return sum(root,0);
+    }
+
+    private int sum(TreeNode node, int num){
+        if(node == null){
+            return 0;
+        }
+        num = num * 10 + node.val;
+
+        if(node.left == null && node.right == null){
+            return num;
+        }
+        return sum(node.left,num) + sum(node.right,num);
+    }
+}
