77cc6c482cf5767e451d2e20ed4f3eb47c3cd641
Vedant Bakshi <vedantbakshi07@gmail.com>
2023-05-09 23:33:35 +0530

    Leetcode #136
    Python solution

---
+++ Single_No.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        for i in range(0, len(nums), 2):
+            if i+1 == len(nums):
+                return nums[i]
+            if nums[i] != nums[i+1]:
+                return nums[i]
+        return nums[-1]
