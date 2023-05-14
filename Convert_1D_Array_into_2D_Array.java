a1d4b4434aa83b2f3e34490306c8cf139d64e31a
Vedant Bakshi <vedantbakshi07@gmail.com>
2023-05-14 23:59:52 +0530

    Update Convert_1D_Array_into_2D_Array.java

--- Convert_1D_Array_into_2D_Array.java
+++ Convert_1D_Array_into_2D_Array.java
@@ -8,6 +8,8 @@
 class Solution {
     public int[][] construct2DArray(int[] original, int m, int n) {
       int [][] arr=new int [m][n];
+        
+        
     if(original.length!=m*n)return new int[0][0];
     int ix=0;
          for(int i=0;i<arr.length;i++) {
