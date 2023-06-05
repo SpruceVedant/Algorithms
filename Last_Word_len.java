e892d917af2698e9209bcea1455540aea8ccd5ad
Vedant Bakshi <vedantbakshi07@gmail.com>
2023-06-05 23:12:13 +0530

    Update Last_Word_len.java

--- Last_Word_len.java
+++ Last_Word_len.java
@@ -9,7 +9,6 @@
 // Output: 5
 // Explanation: The last word is "World" with length 5.
 // Example 2:
-
 // Input: s = "   fly me   to   the moon  "
 // Output: 4
 // Explanation: The last word is "moon" with length 4.
