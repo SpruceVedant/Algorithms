938350e5e0881aac79c890346f4ae5eeb7941e90
Vedant Bakshi <vedantbakshi07@gmail.com>
2024-04-25 18:23:00 +0530

    Create Count_Occurence_of_no.java

---
+++ Count_Occurence_of_no.java
@@ -0,0 +1,29 @@
+// Count occurrences of a number in a sorted array with duplicates
+
+public class count {
+    public int count_occur(int[] arr, int target){
+        int count = 0;
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(arr[mid] == target){
+                count++;
+                start = mid + 1;
+            }else if(arr[mid] <= target){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int arr[] = {2, 5, 5, 5, 6, 6, 8, 9, 9, 9};
+        int target = 8;
+        count count = new count();
+        count.count_occur(arr, target);
+        System.out.println(count.count_occur(arr, target));
+    }
+}
